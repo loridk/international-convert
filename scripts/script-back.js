b8531cbee35ba800bb96c08f401ffb99370c4498
Lori Dunford <lori@redstairs.ca>
2016-04-28 11:20:08 -0400

    Added more converts, new style

---
+++ scripts/script-back.js
@@ -0,0 +1,95 @@
+/**
+ * Created by Lori DK on 10/20/2015.
+ */
+
+// assert if 500 mi is entered
+
+$(document).ready(function() {
+    $("#dis-tab").hide();
+    $("#temp-tab").hide();
+    $("#soon-tab").hide();
+
+
+    $( "#dis" ).on( "click", function( event ) {
+        // Prevent the usual navigation behavior
+        event.preventDefault();
+        $("#dis-tab").show();
+        $("#temp-tab").hide();
+        $("#soon-tab").hide();
+    });
+
+    $( "#temp" ).on( "click", function( event ) {
+        // Prevent the usual navigation behavior
+        event.preventDefault();
+        $("#temp-tab").show();
+        $("#dis-tab").hide();
+        $("#soon-tab").hide();
+    });
+
+    $( "#soon" ).on( "click", function( event ) {
+        // Prevent the usual navigation behavior
+        event.preventDefault();
+        $("#soon-tab").show();
+        $("#dis-tab").hide();
+        $("#temp-tab").hide();
+    });
+
+    
+
+
+    // when Cel is entered
+    $("#celsius").on("keyup",function() {
+        var cel = $('input#celsius').val();
+        degreesF(cel);
+    });
+
+    // convert to Far
+    function degreesF(cel) {
+            convertDegC = cel * 1.8 + 32;
+        console.log('Cel to Far');
+        $("#fahrenheit").val(convertDegC);
+    }
+
+
+    // when Far is entered
+    $("#fahrenheit").on("keyup",function() {
+        var far = $('input#fahrenheit').val();
+        degreesC(far);
+    });
+
+    // convert to Cel
+    function degreesC(far) {
+            convertDegF = (far - 32)  *  5/9;
+        console.log('Far to Cel');
+        $("#celsius").val(convertDegF);
+    }
+
+
+    // when kilometers is entered
+    $("#kilometers").on("keyup",function() {
+        var km = $('input#kilometers').val();
+        distanceM(km);
+    });
+
+    // convert to miles
+    function distanceM(km) {
+            convertKilo = km * 0.62137;
+        $("#miles").val(convertKilo);
+    }
+
+
+    // when miles is entered
+    $("#miles").on("keyup",function() {
+        var mi = $('input#miles').val();
+        distanceK(mi);
+    });
+
+    // convert to kilometers
+    function distanceK(mi) {
+        console.assert((mi != 500), "and I would walk 500 more!");
+            convertMile = mi / 0.62137;
+        console.log('Mi to Km');
+        $("#kilometers").val(convertMile);
+    }
+
+});
--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,17 @@
  * Created by Lori DK on 10/20/2015.
  */
 
+// add more
+// dropdown?
+// bootstrap
+// message
+
 // assert if 500 mi is entered
 
 $(document).ready(function() {
-    $("#dis-tab").hide();
+    $("#dis-tab").show();
     $("#temp-tab").hide();
+    $("#weight-tab").hide();
     $("#soon-tab").hide();
 
 
@@ -15,6 +21,7 @@ $(document).ready(function() {
         event.preventDefault();
         $("#dis-tab").show();
         $("#temp-tab").hide();
+        $("#weight-tab").hide();
         $("#soon-tab").hide();
     });
 
@@ -23,6 +30,16 @@ $(document).ready(function() {
         event.preventDefault();
         $("#temp-tab").show();
         $("#dis-tab").hide();
+        $("#weight-tab").hide();
+        $("#soon-tab").hide();
+    });
+
+    $( "#weight" ).on( "click", function( event ) {
+        // Prevent the usual navigation behavior
+        event.preventDefault();
+        $("#weight-tab").show();
+        $("#dis-tab").hide();
+        $("#temp-tab").hide();
         $("#soon-tab").hide();
     });
 
@@ -32,62 +49,87 @@ $(document).ready(function() {
         $("#soon-tab").show();
         $("#dis-tab").hide();
         $("#temp-tab").hide();
+        $("#weight-tab").hide();
     });
+
+    
 
 
     // when Cel is entered
     $("#celsius").on("keyup",function() {
-        var cel = $('input#celsius').val();
-        degreesF(cel);
+        var cel = $('input#celsius').val(); // input
+        var convertDegC = cel * 1.8 + 32; // formula
+        console.log('Cel to Far'); // log
+        $("#fahrenheit").val(convertDegC); //output
     });
 
-    // convert to Far
-    function degreesF(cel) {
-            convertDegC = cel * 1.8 + 32;
-        console.log('Cel to Far');
-        $("#fahrenheit").val(convertDegC);
-    }
+
 
 
     // when Far is entered
     $("#fahrenheit").on("keyup",function() {
         var far = $('input#fahrenheit').val();
-        degreesC(far);
-    });
-
-    // convert to Cel
-    function degreesC(far) {
-            convertDegF = (far - 32)  *  5/9;
+        var convertDegF = (far - 32)  *  5/9;
         console.log('Far to Cel');
         $("#celsius").val(convertDegF);
-    }
+    });
+
+
 
 
     // when kilometers is entered
     $("#kilometers").on("keyup",function() {
         var km = $('input#kilometers').val();
-        distanceM(km);
-    });
-
-    // convert to miles
-    function distanceM(km) {
-            convertKilo = km * 0.62137;
+        var convertKilo = km * 0.62137;
+        console.log('Km to Mi');
         $("#miles").val(convertKilo);
-    }
+    });
 
 
     // when miles is entered
     $("#miles").on("keyup",function() {
         var mi = $('input#miles').val();
-        distanceK(mi);
-    });
-
-    // convert to kilometers
-    function distanceK(mi) {
         console.assert((mi != 500), "and I would walk 500 more!");
-            convertMile = mi / 0.62137;
+        var convertMile = mi / 0.62137;
         console.log('Mi to Km');
         $("#kilometers").val(convertMile);
-    }
+    });
+
+    // when centemeters is entered
+    $("#centemeters").on("keyup",function() {
+        var cm  = $('input#centemeters').val();
+        var convertCm = cm * 0.39370;
+        console.log('Cm to In');
+        $("#inches").val(convertCm);
+    });
+
+
+    // when inches is entered
+    $("#inches").on("keyup",function() {
+        var inch = $('input#inches').val();
+        var convertIn = inch / 0.39370;
+        console.log('In to Cm');
+        $("#centemeters").val(convertIn);
+    });
+
+
+    // when meters is entered
+    $("#meters").on("keyup",function() {
+        var m  = $('input#meters').val();
+        var convertM = m * 3.2808;
+        console.log('M to Ft');
+        $("#feet").val(convertCm);
+    });
+
+
+    // when feet is entered
+    $("#feet").on("keyup",function() {
+        var foot = $('input#feet').val();
+        var convertFt = foot / 3.2808;
+        console.log('Ft to M');
+        $("#meters").val(convertIn);
+    });
+
+
 
 });
